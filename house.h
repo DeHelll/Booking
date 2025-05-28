98c279a477c5a78ecc9298278df2ae536d47ce16
Artur Kucher <arturkucer068@gmail.com>
2025-05-28 11:44:04 +0200

    Create class house

--- house.h
+++ house.h
@@ -3,3 +3,27 @@
 #include <memory>
 #include <vector>
 #include <string>
+
+class House {
+protected:
+	std::string address;
+	double pricePerNight;
+	bool isAvailable;
+
+public:
+	House(const std::string& _address, double _pricePerNight)
+		: address(_address), pricePerNight(_pricePerNight), isAvailable(true){}
+
+	virtual ~House() = default;
+
+	virtual void display() const {
+		std::cout << "Address: " << address
+			<< "\nPrice per night: $" << pricePerNight
+			<< "\nStatus: " << (isAvailable ? "Available" : "Booked") << "\n\n";
+	}
+
+
+
+
+
+};
